78be0f676cb00c97f4b61d65fc860aed3e2fa3bb
Kajal Saini <sizukabts91@gmail.com>
2025-09-22 16:30:15 +0530

    cookie read and delete

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,15 @@ router.get('/', function (req, res) {
   res.render("index");
 });
 
+router.get('/read', function (req, res) {
+ console.log(req.cookies.age);
+ res.send("check")
+});
 
+router.get('/delete', function (req, res) {
+  res.clearCookie("age")
+  res.send("clear ho gyi")
+});
 
 
 module.exports = router;
